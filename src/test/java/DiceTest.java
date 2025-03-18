0f3c28e0f6373873781c54c2f83a63aba6f2530c
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-03-18 14:57:52 -0400

    testing testing

--- src/test/java/DiceTest.java
+++ src/test/java/DiceTest.java
@@ -1,6 +1,37 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DiceTest {
+    Dice dice;
+    Bins bins;
+    Simulation simulation;
 
+    @BeforeEach
+    public void setUp(){
+        dice = new Dice(2);
+        bins = new Bins(2,12);
+        simulation = new Simulation(2,1000 );
+    }
+    @Test
+    public void testDiceRollWithinRange(){
+        //Given
+        Dice dice = new Dice(2);
+        //When
+        int result=dice.rollAdd();
+        //Then
+        Assert.assertTrue(result>=2&&result<=12);
+    }
+    @Test
+    public void binsTest() {
+        //Given
+        Bins bins = new Bins(2, 12);
+        //when
+        for (int i = 2; i <= 12; i++) {
+            //Then
+                assertEquals(0, bins.getBin(i), "Bin " + i + " should start at 0.");
+        }
+    }
 }
